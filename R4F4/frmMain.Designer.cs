4727793cd55fd75f4b54f39bd1a4890bd7e01394
orthoxerox <orthoxerox@gmail.com>
2015-12-14 20:59:22 +0300

    Basic file management infrastructure

--- R4F4/frmMain.Designer.cs
+++ R4F4/frmMain.Designer.cs
@@ -27,28 +27,113 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.txtOutput = new System.Windows.Forms.TextBox();
+            this.mnuMain = new System.Windows.Forms.MenuStrip();
+            this.mnuFile = new System.Windows.Forms.ToolStripMenuItem();
+            this.miOpen = new System.Windows.Forms.ToolStripMenuItem();
+            this.miSave = new System.Windows.Forms.ToolStripMenuItem();
+            this.miSaveAs = new System.Windows.Forms.ToolStripMenuItem();
+            this.miExit = new System.Windows.Forms.ToolStripMenuItem();
+            this.mnuHelp = new System.Windows.Forms.ToolStripMenuItem();
+            this.mnuAbout = new System.Windows.Forms.ToolStripMenuItem();
+            this.dlgSave = new System.Windows.Forms.SaveFileDialog();
+            this.dlgOpen = new System.Windows.Forms.OpenFileDialog();
+            this.dlgFolder = new System.Windows.Forms.FolderBrowserDialog();
+            this.mnuMain.SuspendLayout();
             this.SuspendLayout();
             // 
-            // txtOutput
+            // mnuMain
             // 
-            this.txtOutput.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.txtOutput.Location = new System.Drawing.Point(0, 0);
-            this.txtOutput.Multiline = true;
-            this.txtOutput.Name = "txtOutput";
-            this.txtOutput.ScrollBars = System.Windows.Forms.ScrollBars.Both;
-            this.txtOutput.Size = new System.Drawing.Size(1368, 1007);
-            this.txtOutput.TabIndex = 0;
+            this.mnuMain.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.mnuFile,
+            this.mnuHelp});
+            this.mnuMain.Location = new System.Drawing.Point(0, 0);
+            this.mnuMain.Name = "mnuMain";
+            this.mnuMain.Size = new System.Drawing.Size(912, 24);
+            this.mnuMain.TabIndex = 0;
+            this.mnuMain.Text = "menuStrip1";
+            // 
+            // mnuFile
+            // 
+            this.mnuFile.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.miOpen,
+            this.miSave,
+            this.miSaveAs,
+            this.miExit});
+            this.mnuFile.Name = "mnuFile";
+            this.mnuFile.Size = new System.Drawing.Size(37, 20);
+            this.mnuFile.Text = "&File";
+            // 
+            // miOpen
+            // 
+            this.miOpen.Name = "miOpen";
+            this.miOpen.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.O)));
+            this.miOpen.Size = new System.Drawing.Size(155, 22);
+            this.miOpen.Text = "&Open...";
+            this.miOpen.Click += new System.EventHandler(this.miOpen_Click);
+            // 
+            // miSave
+            // 
+            this.miSave.Enabled = false;
+            this.miSave.Name = "miSave";
+            this.miSave.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.S)));
+            this.miSave.Size = new System.Drawing.Size(155, 22);
+            this.miSave.Text = "&Save";
+            this.miSave.Click += new System.EventHandler(this.miSave_Click);
+            // 
+            // miSaveAs
+            // 
+            this.miSaveAs.Enabled = false;
+            this.miSaveAs.Name = "miSaveAs";
+            this.miSaveAs.ShortcutKeys = System.Windows.Forms.Keys.F12;
+            this.miSaveAs.Size = new System.Drawing.Size(155, 22);
+            this.miSaveAs.Text = "Save &As...";
+            this.miSaveAs.Click += new System.EventHandler(this.miSaveAs_Click);
+            // 
+            // miExit
+            // 
+            this.miExit.Name = "miExit";
+            this.miExit.Size = new System.Drawing.Size(155, 22);
+            this.miExit.Text = "E&xit";
+            this.miExit.Click += new System.EventHandler(this.miExit_Click);
+            // 
+            // mnuHelp
+            // 
+            this.mnuHelp.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.mnuAbout});
+            this.mnuHelp.Name = "mnuHelp";
+            this.mnuHelp.Size = new System.Drawing.Size(44, 20);
+            this.mnuHelp.Text = "&Help";
+            // 
+            // mnuAbout
+            // 
+            this.mnuAbout.Name = "mnuAbout";
+            this.mnuAbout.ShortcutKeys = System.Windows.Forms.Keys.F1;
+            this.mnuAbout.Size = new System.Drawing.Size(135, 22);
+            this.mnuAbout.Text = "&About...";
+            this.mnuAbout.Click += new System.EventHandler(this.mnuAbout_Click);
+            // 
+            // dlgOpen
+            // 
+            this.dlgOpen.FileName = "CustomControlMap.txt";
+            // 
+            // dlgFolder
+            // 
+            this.dlgFolder.SelectedPath = "C:\\Program Files";
+            this.dlgFolder.ShowNewFolderButton = false;
             // 
             // frmMain
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1368, 1007);
-            this.Controls.Add(this.txtOutput);
+            this.ClientSize = new System.Drawing.Size(912, 655);
+            this.Controls.Add(this.mnuMain);
+            this.MainMenuStrip = this.mnuMain;
+            this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "frmMain";
-            this.Text = "Form1";
+            this.Text = "R4F4";
             this.Load += new System.EventHandler(this.frmMain_Load);
+            this.mnuMain.ResumeLayout(false);
+            this.mnuMain.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -56,7 +141,17 @@
 
         #endregion
 
-        private System.Windows.Forms.TextBox txtOutput;
+        private System.Windows.Forms.MenuStrip mnuMain;
+        private System.Windows.Forms.ToolStripMenuItem mnuFile;
+        private System.Windows.Forms.ToolStripMenuItem miOpen;
+        private System.Windows.Forms.ToolStripMenuItem miSave;
+        private System.Windows.Forms.ToolStripMenuItem miSaveAs;
+        private System.Windows.Forms.ToolStripMenuItem miExit;
+        private System.Windows.Forms.ToolStripMenuItem mnuHelp;
+        private System.Windows.Forms.ToolStripMenuItem mnuAbout;
+        private System.Windows.Forms.SaveFileDialog dlgSave;
+        private System.Windows.Forms.OpenFileDialog dlgOpen;
+        private System.Windows.Forms.FolderBrowserDialog dlgFolder;
     }
 }
 
--- R4F4/frmMain.cs
+++ R4F4/frmMain.cs
@@ -9,11 +9,31 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.IO;
 using R4F4.Model;
+using System.Configuration;
 
 namespace R4F4
 {
     public partial class frmMain : Form
     {
+        string fileName;
+        string falloutPath;
+        bool edited = false;
+        ControlScheme scheme;
+
+        public bool Edited
+        {
+            get
+            {
+                return edited;
+            }
+
+            set
+            {
+                this.edited = value;
+                miSave.Enabled = value;
+            }
+        }
+
         public frmMain()
         {
             InitializeComponent();
@@ -21,16 +41,113 @@ namespace R4F4
 
         private void frmMain_Load(object sender, EventArgs e)
         {
-            var bs = new ControlScheme(
-                File.ReadAllText(
-                    @"CustomControlMap.txt"));
-            foreach (var b in bs.Bindings) {
-                txtOutput.AppendText(b.ToString());
-                txtOutput.AppendText("\r\n");
+            falloutPath = ConfigurationManager.AppSettings["falloutPath"];
+            if (falloutPath == "?") UpdateFalloutPath();
+            dlgOpen.InitialDirectory = falloutPath;
+        }
+
+        private void UpdateFalloutPath()
+        {
+            MessageBox.Show(@"Please select the F4SE folder inside your Fallout 4\Data directory:");
+            dlgFolder.ShowDialog();
+            falloutPath = dlgFolder.SelectedPath;
+            ConfigurationManager.AppSettings["falloutPath"] = falloutPath;
+        }
+
+        private void mnuAbout_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(
+@"R4F4 © 2015 orthoxerox.
+Licensed under GNU AGPL v3.
+
+Fallout, Prepare for the Future 
+and related logos are trademarks 
+or registered trademarks of 
+Bethesda Softworks LLC 
+in the U.S. and/or other countries.
+All Rights Reserved.",
+"R4F4");
+        }
+
+        private void miOpen_Click(object sender, EventArgs e)
+        {
+            if (Edited) {
+                switch (MessageBox.Show(
+                    "Are you sure you want to discard unsaved changes?", 
+                    "R4F4", 
+                    MessageBoxButtons.YesNo)) {
+                    case DialogResult.No:
+                        return;
+                    default:
+                        break;
+                }
             }
 
-            txtOutput.AppendText("============\r\n");
-            txtOutput.AppendText(bs.ToString());
+            switch (dlgOpen.ShowDialog()) {
+                case DialogResult.OK:
+                    fileName = dlgOpen.FileName;
+                    var oldScheme = scheme;
+                    try {
+                        scheme = new ControlScheme(File.ReadAllText(fileName));
+                    }
+                    catch (Exception) {
+                        MessageBox.Show("Error opening file");
+                    }
+                    if (scheme.Errors.Count > 0) {
+                        MessageBox.Show("Invalid format!");
+                        scheme = oldScheme;
+                        return;
+                    }
+                    Edited = false;
+                    miSaveAs.Enabled = true;
+                    PopulateGrid();
+                    return;
+                default:
+                    return;
+            }
+        }
+
+        private void PopulateGrid()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void miExit_Click(object sender, EventArgs e)
+        {
+            if (Edited) {
+                switch (MessageBox.Show(
+                    "Are you sure you want to discard unsaved changes?",
+                    "R4F4",
+                    MessageBoxButtons.YesNo)) {
+                    case DialogResult.No:
+                        return;
+                    default:
+                        break;
+                }
+            }
+
+            Application.Exit();
+        }
+
+        private void miSave_Click(object sender, EventArgs e)
+        {
+            File.WriteAllText(fileName, scheme.ToString());
+        }
+
+        private void miSaveAs_Click(object sender, EventArgs e)
+        {
+            dlgSave.InitialDirectory = Path.GetFullPath(fileName);
+            dlgSave.FileName = fileName;
+            switch (dlgSave.ShowDialog()) {
+                case DialogResult.OK:
+                    fileName = dlgSave.FileName;
+                    Edited = false;
+                    File.WriteAllText(fileName, scheme.ToString());
+                    return;
+                default:
+                    return;
+            }
+
         }
     }
 }
